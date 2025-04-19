22aad5a1104cf6810c32954c0449cb4ab153e1cd
Maxwell Tang <91094317+mackstey28@users.noreply.github.com>
2025-04-19 16:35:42 -0400

    "M" line tracing SVG
    This was dummy hard to make

--- src/animations/path-drawing.tsx
+++ src/animations/path-drawing.tsx
@@ -19,149 +19,85 @@ const draw = {
 
 export function PathDrawing() {
     return (
-        <motion.svg
-            width="600"
-            height="600"
-            viewBox="0 0 600 600"
-            initial="hidden"
-            animate="visible"
-            style={image}
-        >
-            <motion.circle
-                className="circle-path"
-                cx="100"
-                cy="100"
-                r="80"
-                stroke="#ff0088"
-                variants={draw}
-                custom={1}
-                style={shape}
-            />
-            <motion.line
-                x1="220"
-                y1="30"
-                x2="360"
-                y2="170"
-                stroke="#8df0cc"
-                variants={draw}
-                custom={2}
-                style={shape}
-            />
-            <motion.line
-                x1="220"
-                y1="170"
-                x2="360"
-                y2="30"
-                stroke="#8df0cc"
-                variants={draw}
-                custom={2.5}
-                style={shape}
-            />
-            <motion.rect
-                width="140"
-                height="140"
-                x="410"
-                y="30"
-                rx="20"
-                stroke="#0d63f8"
-                variants={draw}
-                custom={3}
-                style={shape}
-            />
-            <motion.circle
-                cx="100"
-                cy="300"
-                r="80"
-                stroke="#0d63f8"
-                variants={draw}
-                custom={2}
-                style={shape}
-            />
-            <motion.line
-                x1="220"
-                y1="230"
-                x2="360"
-                y2="370"
-                stroke="#ff0088"
-                custom={3}
-                variants={draw}
-                style={shape}
-            />
-            <motion.line
-                x1="220"
-                y1="370"
-                x2="360"
-                y2="230"
-                stroke="#ff0088"
-                custom={3.5}
-                variants={draw}
-                style={shape}
-            />
-            <motion.rect
-                width="140"
-                height="140"
-                x="410"
-                y="230"
-                rx="20"
-                stroke="#8df0cc"
-                custom={4}
-                variants={draw}
-                style={shape}
-            />
-            <motion.circle
-                cx="100"
-                cy="500"
-                r="80"
-                stroke="#8df0cc"
-                variants={draw}
-                custom={3}
-                style={shape}
-            />
-            <motion.line
-                x1="220"
-                y1="430"
-                x2="360"
-                y2="570"
-                stroke="#0d63f8"
-                variants={draw}
-                custom={4}
-                style={shape}
-            />
-            <motion.line
-                x1="220"
-                y1="570"
-                x2="360"
-                y2="430"
-                stroke="#0d63f8"
-                variants={draw}
-                custom={4.5}
-                style={shape}
-            />
-            <motion.rect
-                width="140"
-                height="140"
-                x="410"
-                y="430"
-                rx="20"
-                stroke="#ff0088"
-                variants={draw}
-                custom={5}
-                style={shape}
-            />
-        </motion.svg>
+        <div className="centerText">
+            <motion.svg
+                width="1920"
+                height="600"
+                viewBox="0 0 600 600"
+                initial="hidden"
+                animate="visible"
+            >
+                <motion.line // first stoke of M
+                    x1="0"
+                    y1="160"
+                    x2="0"
+                    y2="10"
+                    stroke="#ffffff"
+                    variants={draw}
+                    custom={0}
+                    style={round}
+                />
+                <motion.line // second stroke of M
+                    x1="1.09"
+                    y1="13.9"
+                    x2="66"
+                    y2="120"
+                    stroke="#ffffff"
+                    variants={draw}
+                    custom={2}
+                    style={round}
+                />
+                <motion.line // third stroke of M
+                    x1="54"
+                    y1="120"
+                    x2="118.91"
+                    y2="13.9"
+                    stroke="#ffffff"
+                    variants={draw}
+                    custom={4}
+                    style={round}
+                />
+                <motion.line // black stroke blocking second stroke of M
+                    x1="20.68"
+                    y1="74.34"
+                    x2="53.18"
+                    y2="127.79"
+                    stroke="#0a0a0a"
+                    variants={draw}
+                    custom={0}
+                    style={round}
+                />
+                <motion.line // black stroke blocking third stroke of M
+                    x1="66.82"
+                    y1="127.79"
+                    x2="99.32"
+                    y2="74.34"
+                    stroke="#0a0a0a"
+                    variants={draw}
+                    custom={0}
+                    style={round}
+                />
+                <motion.line // fourth stroke of M
+                    x1="120"
+                    y1="10"
+                    x2="120"
+                    y2="160"
+                    stroke="#ffffff"
+                    variants={draw}
+                    custom={6}
+                    style={round}
+                />
+            </motion.svg>
+        </div>
     )
 }
 
 /**
  * ==============   Styles   ================
  */
-
-const image: React.CSSProperties = {
-    maxWidth: "80vw",
-}
-
-const shape: React.CSSProperties = {
-    strokeWidth: 10,
-    strokeLinecap: "round",
+const round: React.CSSProperties = {
+    strokeWidth: 15,
+    strokeLinecap: "butt",
+    strokeLinejoin: "miter",
     fill: "transparent",
 }
