e8a6815618d432e7464cc50d8ed5357e4f49e98e
Maxwell Tang <91094317+mackstey28@users.noreply.github.com>
2025-04-28 15:59:36 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",  // <=== enables static exports
-  // basePath: "/personal-website",
+  basePath: "/personal-website",
+  assetPrefix: '/personal-website',
   reactStrictMode: true,
 };
 
+module.exports = nextConfig;
 export default nextConfig;
